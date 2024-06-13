14286c87e2fe10613f4c58919f70a61903586f01
eswarasamy <eswarasamy87@gmail.com>
2024-06-13 14:44:46 +0530

    File added

--- Palindrome/App.js
+++ Palindrome/App.js
@@ -9,22 +9,25 @@ function isPalindrome(str) {
     left++;
     right--;
   }
-
   return true;
 }
 
-// Test with different strings
 let testString1 = "true";
 let result1 = isPalindrome(testString1);
-console.log(result1);
+console.log(`${testString1} Is a palindrome: ${result1}`);
 
 let testString2 = "A man, a plan, a canal, Panama!";
 let result2 = isPalindrome(testString2);
-console.log(result2);
+console.log(`${testString2} Is a palindrome: ${result2}`);
 
 let testString3 = "racecar";
 let result3 = isPalindrome(testString3);
-console.log(result3);
+console.log(`${testString3} Is a palindrome: ${result3}`);
+
 let testString4 = "Was it a car or a cat I saw?";
 let result4 = isPalindrome(testString4);
-console.log(result4);
+console.log(`${testString4} Is a palindrome: ${result4}`);
+
+let testString5 = "Don't come";
+let result5 = isPalindrome(testString5);
+console.log(`${testString5} Is a palindrome: ${result5}`);
